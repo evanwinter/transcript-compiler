823b153a810d354813d3884a53c0a685df64de2a
Evan Winter <ewinter1996@gmail.com>
2018-02-06 03:31:03 -0600

    documentation and comments

--- main.py
+++ main.py
@@ -5,28 +5,36 @@ import os
 import re
 from bs4 import BeautifulSoup
 
+# Find the show on https://www.springfieldspringfield.co.uk/tv_show_episode_scripts.php and use the series
+# title found at the end of the URL. Some have the year and some don't!
 series_title = 'black-mirror-2011'
 base_url = 'https://www.springfieldspringfield.co.uk/'
-series_url = 'episode_scripts.php?tv-show=' + series_title
+series_url = 'episode_scripts.php?tv-show=' + series_title	# construct the url for this series' transcripts
 
 response = requests.get(base_url + series_url)
 html = BeautifulSoup(response.text, 'html.parser')
 episodes = html.find_all('a', class_='season-episode-title')
 count = 0
 
+# Prepare output directory and file
 if not os.path.exists('output/'):
 	os.makedirs('output/')
-
 output_file = 'output/' + series_title + '.txt'
 
 with open(output_file, 'w') as f:
 	f.write('Transcripts for ' + series_title.upper() + '\n\n')
 
+	# For each episode of this series, write the transcript to the output file
 	for episode in episodes:
-		
+
+		# Get the episode url
 		episode_url = episode.get('href')
 		episode_page = requests.get(base_url + episode_url)
+		
+		 # Parse the HTML at that url
 		episode_html = BeautifulSoup(episode_page.text, 'html.parser')
+		
+		# Check for the episode title and transcript
 		try:
 			episode_title = episode_html.find('div', class_='breadcrumbs').findNext('h3').get_text()
 		except:
@@ -38,11 +46,9 @@ with open(output_file, 'w') as f:
 			print('--Error getting transcript.')
 			pass
 
-		print(episode_title)
-
-		# Write to file
+		# Write title and transcript to output file
 		f.write('Title: ' + episode_title + '\n')
 		f.write('-----------------------\n')
 		f.write(episode_transcript + '\n\n')
 
-print('Done!')
+sys.exit(-1)
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Transcript Compiler
+
+Compiles entire television series' transcripts as they appear on crowd-sourced TV and movie transcript database website ['Springfield! Springfield!'](https://www.springfieldspringfield.co.uk/tv_show_episode_scripts.php).
+
+To run:
+
+`git clone https://github.com/evanwinter/transcript-compiler`
+
+`cd transcript-compiler`
+
+`pip install` the dependencies.
+
+`python3 main.py`
+
+Transcripts will be stored in the following location:
+
+`transcript-compiler/output/{series-title}.txt`
